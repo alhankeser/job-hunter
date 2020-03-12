1c219ab4996156fbed712b81efa451440c2521ac
alhankeser <alhankeser@gmail.com>
2020-03-12 17:04:42 -0400

    New companies

--- job_pages.py
+++ job_pages.py
@@ -64,5 +64,113 @@ job_pages = [
     {
         "url": "https://www.helpscout.com/company/careers/",
         "path": "lever.co/helpscout",
+    },
+    {
+        "url": "https://jobs.helpdocs.io/",
+        "path": "/o/",
+    },
+    {
+        "url": "https://www.stickermule.com/careers",
+        "path": "/career/",
+    },
+    {
+        "url": "https://www.fastly.com/about/careers",
+        "path": "/about/jobs/",
+    },
+    {
+        "url": "https://www.aliceplatform.com/careers",
+        "path": "/apply/",
+    },
+    {
+        "url": "https://careers.redventures.com/positions.html",
+        "path": "/application/position/",
+    },
+    {
+        "url": "https://wildbit.com/jobs",
+        "path": "/jobs/",
+    },
+    {
+        "url": "https://www.osano.com/company/careers",
+        "path": "/jobs/",
+    },
+    {
+        "url": "https://careers.snapeda.com/",
+        "path": "/o/",
+    },
+    {
+        "url": "https://www.humio.com/careers/",
+        "path": "/humio/jobs/",
+    },
+    {
+        "url": "https://www.getvero.com/careers/",
+        "path": "workable.com/getvero/",
+    },
+    {
+        "url": "https://www.knock.com/careers#current-openings",
+        "path": "/knock/",
+    },
+    {
+        "url": "https://wesrom.com/current-openings",
+        "path": "/career/",
+    },
+    {
+        "url": "https://careers.jobscore.com/careers/beekeepermarketing",
+        "path": "/jobs/",
+    },
+    {
+        "url": "https://join.xwp.co/",
+        "path": "/o/",
+    },
+    {
+        "url": "https://careers.ghost.org/",
+        "path": "/en",
+    },
+    {
+        "url": "https://careers.vidiq.com/",
+        "path": "/o/",
+    },
+    {
+        "url": "https://www.thorn.org/careers/",
+        "path": "/application/",
+    },
+    {
+        "url": "https://everfi.com/careers/",
+        "path": "/careers/",
+    },
+    {
+        "url": "https://www.gaiagps.com/company/jobs/",
+        "path": "/jobs/",
+    },
+    {
+        "url": "https://bitfinex.recruitee.com/",
+        "path": "/o/",
+    },
+    {
+        "url": "https://www.formstack.com/careers",
+        "path": "/apply/",
+    },
+    {
+        "url": "https://www.gocrisp.com/careers-current-openings",
+        "path": "/careers-current-openings",
+    },
+    {
+        "url": "https://apply.workable.com/drops/",
+        "path": "/drops/",
+    },
+    {
+        "url": "https://apply.workable.com/formassembly/",
+        "path": "/j/",
+    },
+    {
+        "url": "https://careers.nielsen.com/en-us/?s&post_type=openings&regions=europe&locations=europe&teams=analytics%2Cdata-science&types=regular&schedules=full-time&orderby&order&Europe=europe&ame&asia-pacific&europe=europe&greater-china&india&latam&na&Analytics=analytics&Data_Science=data-science&Regular=regular&Full-Time=full-time",
+        "path": "/job/",
+    },
+    {
+        "url": "https://www.aha.io/company/careers/current-openings",
+        "path": "/careers/",
+    },
+    {
+        "url": "https://www.digital-science.com/jobs/",
+        "path": "/jobs/"
     }
 ]
